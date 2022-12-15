5f36f1a7439d21df9d4930021c7dc3298073c002
Jed Harwood <scaryteeth@hotmail.com>
2022-12-15 09:47:25 +0900

    set css file in separate folder

--- hacker-news-story-ui-react/src/index.tsx
+++ hacker-news-story-ui-react/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './css/index.css'
 
 const container = document.getElementById('app-root')!
 const root = createRoot(container)
-root.render(<h1>Hello React!</h1>)
+root.render(<h1>Hacker News!</h1>)
