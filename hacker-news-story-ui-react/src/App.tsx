73fe2a56d86d21dd8b08a94bf24f889385434fa2
Jed Harwood <scaryteeth@hotmail.com>
2022-12-16 09:24:03 +0900

    linting fixes

--- hacker-news-story-ui-react/src/App.tsx
+++ hacker-news-story-ui-react/src/App.tsx
@@ -1,18 +1,18 @@
-import React from "react";
+import React from 'react';
 import { Stories } from './components/Stories';
 import styled from 'styled-components';
 import { Header } from './components/Header';
 
 const Main = styled.main`
   min-height: 100vh;
-  background-color: #EEEEE4;
+  background-color: #eeeee4;
 `;
 
 const Section = styled.section`
   min-height: 100vh;
   max-width: 75%;
   margin: auto;
-  background-color: #154C79;
+  background-color: #154c79;
   padding: 0px 20px 1px 20px;
 `;
 
--- hacker-news-story-ui-react/src/api/Fetch.ts
+++ hacker-news-story-ui-react/src/api/Fetch.ts
@@ -1,27 +1,27 @@
 export interface Story {
-    by: string;
-    id: number;
-    kids: number[];
-    score: number;
-    title: string;
-    url: string;
-    order: number;
+  by: string;
+  id: number;
+  kids: number[];
+  score: number;
+  title: string;
+  url: string;
+  order: number;
 }
-  
+
 export const getStories = async (): Promise<Story[]> => {
-    const storyIds = await fetch(
-        "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty"
-    ).then(res => res.json());
-    const stories: any = await Promise.all(
-        storyIds.slice(0, 100).map(
-        async (id: number, idx: number): Promise<Story> => {
-            const story: Story = await fetch(
-                `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`
-            ).then(res => res.json());
-            story.order = idx + 1;
-            return story;
-        }
-        )
-    );
-    return stories.sort((a: Story, b: Story) => a.order - b.order);
+  const storyIds = await fetch(
+    'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty'
+  ).then((res) => res.json());
+  const stories: any = await Promise.all(
+    storyIds
+      .slice(0, 100)
+      .map(async (id: number, idx: number): Promise<Story> => {
+        const story: Story = await fetch(
+          `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`
+        ).then((res) => res.json());
+        story.order = idx + 1;
+        return story;
+      })
+  );
+  return stories.sort((a: Story, b: Story) => a.order - b.order);
 };
